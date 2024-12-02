e16bdc61a3211a721d6292e454fa7fd10d745615
jonathan <jongeak.choi@ucalgary.ca>
2024-12-02 06:34:46 -0700

    Variable user menu depending on type of user

--- src/components/navigation_drawer/hairdresser_usermenu/index.jsx
+++ src/components/navigation_drawer/hairdresser_usermenu/index.jsx
@@ -28,7 +28,6 @@ export default function HairDresserUserMenu( { handleClick, anchorEl, setAnchorE
         setAnchorEl(null);
     };
 
-    const handleReturn = () => navigate("/");
     const handleFavorite = () => navigate("/favorites");
     const handleLogin = () => navigate("/auth/login");
     const handleLogout = () => {
@@ -56,35 +55,18 @@ export default function HairDresserUserMenu( { handleClick, anchorEl, setAnchorE
         return color;
       }
       
-      function stringAvatar(name) {
+    function stringAvatar(name) {
         return {
-          sx: {
+            sx: {
             bgcolor: stringToColor(name),
-          },
-          children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+            },
+            children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
         };
-      }
+    }
 
-    if (user.signedIn) {
-        const name = sessionStorage.getItem("name") + " " + sessionStorage.getItem("lastname");
-
-        return (
-            <div style={{marginLeft: 'auto'}}>
-            <Fragment>
-                <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-                    <Tooltip title="Account settings">
-                        <IconButton
-                            onClick={handleClick}
-                            size="small"
-                            sx={{ ml: 2 }}
-                            aria-controls={open ? 'account-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={open ? 'true' : undefined}
-                        >
-                            <Avatar sx={{ width: '3rem', height: '3rem' }} {...stringAvatar(name)} />
-                        </IconButton>
-                    </Tooltip>
-                </Box>
+    const MenuSelector = () => {
+        if (user.userType === "stylist") {
+            return (
                 <Menu
                     anchorEl={anchorEl}
                     id="account-menu"
@@ -125,18 +107,65 @@ export default function HairDresserUserMenu( { handleClick, anchorEl, setAnchorE
                     <MenuItem onClick={handleClose}>
                         <Avatar sx={{ width: '3rem', height: '3rem' }} {...stringAvatar(name)} /> Your Profile Page
                     </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                            <Settings fontSize="small" />
+                        </ListItemIcon>
+                        Settings
+                    </MenuItem>
+                    <MenuItem onClick={handleLogout}>
+                        <ListItemIcon>
+                            <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Logout
+                    </MenuItem>
+                </Menu>
+            )
+        } else {
+            return (
+                <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    slotProps={{
+                        paper: {
+                            elevation: 0,
+                            sx: {
+                                overflow: 'visible',
+                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                mt: 1.5,
+                                '& .MuiAvatar-root': {
+                                    width: 32,
+                                    height: 32,
+                                    ml: -0.5,
+                                    mr: 1,
+                                },
+                                '&::before': {
+                                    content: '""',
+                                    display: 'block',
+                                    position: 'absolute',
+                                    top: 0,
+                                    right: 14,
+                                    width: 10,
+                                    height: 10,
+                                    bgcolor: 'background.paper',
+                                    transform: 'translateY(-50%) rotate(45deg)',
+                                    zIndex: 0,
+                                },
+                            },
+                        },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
                     <MenuItem onClick={handleFavorite}>
                         <ListItemIcon>
                             <Bookmarks fontSize="small" />
                         </ListItemIcon>
                         Favorite Profiles
-                    </MenuItem>
-                    <Divider />
-                    <MenuItem onClick={handleReturn}>
-                        <ListItemIcon>
-                            <PersonSearch fontSize="small" />
-                        </ListItemIcon>
-                        Return to Main Page
                     </MenuItem>
                     <Divider />
                     <MenuItem onClick={handleClose}>
@@ -152,6 +181,31 @@ export default function HairDresserUserMenu( { handleClick, anchorEl, setAnchorE
                         Logout
                     </MenuItem>
                 </Menu>
+            )
+        }
+    }
+
+    if (user.signedIn) {
+        const name = sessionStorage.getItem("name") + " " + sessionStorage.getItem("lastname");
+
+        return (
+            <div style={{marginLeft: 'auto'}}>
+            <Fragment>
+                <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                    <Tooltip title="Account settings">
+                        <IconButton
+                            onClick={handleClick}
+                            size="small"
+                            sx={{ ml: 2 }}
+                            aria-controls={open ? 'account-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                        >
+                            <Avatar sx={{ width: '3rem', height: '3rem' }} {...stringAvatar(name)} />
+                        </IconButton>
+                    </Tooltip>
+                </Box>
+                <MenuSelector />
             </Fragment>
             </div>
         )
